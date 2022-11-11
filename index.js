bc64a7b28f4a88b8b34831158d35024eed2b89f2
GambuzX <mg_mesquita@hotmail.com>
2022-11-11 21:57:57 +0000

    any type

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
 }
 greet("Brendan", new Date());
+var obj = { x: 0 };
+obj.foo();
+obj();
+obj.bar = 100;
+obj = "hello";
+var n = obj;
--- index.ts
+++ index.ts
@@ -3,3 +3,10 @@ function greet(person: string , date: Date) {
 }
 
 greet("Brendan", new Date());
+
+let obj: any = { x: 0 };
+obj.foo();
+obj();
+obj.bar = 100;
+obj = "hello";
+const n: number = obj;
